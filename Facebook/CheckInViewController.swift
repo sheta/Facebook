9491e942312e56a9a71230bdc427d0071af24977
sheta <sheta.chatterjee@gmail.com>
2014-09-08 19:29:57 -0700

    Assignment #1 iOS for Designers
    Sheta Chatterjee
    Facebook App
    Assignment #1
    Yahoo

---
+++ Facebook/CheckInViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CheckInViewController.swift
+//  Facebook
+//
+//  Created by Sheta Chatterjee on 9/8/14.
+//  Copyright (c) 2014 Sheta Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class CheckInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onCancelButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+ 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Facebook/ThrillistViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ThrillistViewController.swift
+//  Facebook
+//
+//  Created by Sheta Chatterjee on 9/7/14.
+//  Copyright (c) 2014 Sheta Chatterjee. All rights reserved.
+//
+
+import UIKit
+
+class ThrillistViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        scrollView.contentSize = CGSize(width: 320, height: 1000)
+         scrollView.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onBackButton(sender: AnyObject) {
+        navigationController!.popViewControllerAnimated(true)
+    }
+
+    
+    
+    @IBAction func onLikeButton(sender: AnyObject) {
+        likeButton.selected = !likeButton.selected
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
